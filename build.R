f13562fa6032e6204a8fa098363e9280f59f04c8
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-12 15:58:38 +1000

    adds examples and rebuilds

--- build.R
+++ build.R
@@ -1,9 +1,15 @@
 # requires an installation of sass
+
 build_strapless <- function() {
+
   wd <- setwd(here::here("strapless","scss"))
   on.exit(setwd(wd))
+
   source <- here::here("strapless", "scss", "strapless.scss")
   output <- here::here("strapless", "css", "strapless.css")
+
   system(paste("sass", source, output))
+  file.copy(output, here::here("examples", "distill", "strapless.css"))
 }
+
 build_strapless()
