727a53aedd0eb883ec15fd74777f67f9fef9ab22
Danielle Navarro <d.navarro@unsw.edu.au>
2021-08-12 13:40:22 +1000

    simplifies variables

--- build.R
+++ build.R
@@ -1,5 +1,9 @@
 # requires an installation of sass
-
-source <- here::here("strapless", "scss", "strapless.scss")
-output <- here::here("strapless", "css", "strapless.css")
-system(paste("sass", source, output))
+build_strapless <- function() {
+  wd <- setwd(here::here("strapless","scss"))
+  on.exit(setwd(wd))
+  source <- here::here("strapless", "scss", "strapless.scss")
+  output <- here::here("strapless", "css", "strapless.css")
+  system(paste("sass", source, output))
+}
+build_strapless()
